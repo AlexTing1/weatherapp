bdc71ab5eeb30179c19150702a10467c30c19e66
Alex Ting <alexting6366@gmail.com>
2025-07-23 22:40:50 -0700

    added ref to form

--- client/src/Components/CityWeather.jsx
+++ client/src/Components/CityWeather.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import styles from '../styles/CityWeather.module.css';
 
 function CityWeather({ temperature, city, wind }) {
@@ -8,12 +8,25 @@ function CityWeather({ temperature, city, wind }) {
     const [temperatureText, setTemperatureText] = useState(temperature);
     const [windText, setWindText] = useState(wind);
 
+    const formRef = useRef();
+
     function handleClick(e) {
         setToEdit(!toEdit);
     }
 
+    useEffect(() => {
+        function handleClickOutside(e) {
+            if (formRef.current && !formRef.current.contains(e.target)) {
+                setToEdit(false);
+            }
+        }
+
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => document.removeEventListener('mousedown', handleClickOutside);
+    }, []);
+
     return (
-        <div className={styles.weatherContainer} onClick={handleClick}>
+        <div className={styles.weatherContainer} onClick={handleClick} ref={formRef}>
             <div className={styles.city}>
                 {!toEdit ? <h1>{cityText}</h1> : <input value={cityText} onChange={(e) => setCityText(e.target.value)} autoFocus />}
             </div>
